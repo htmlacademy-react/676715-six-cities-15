c7768fb79b4ff95ee4a1413636db4ca97722eb67
dinara-k <anidafl87@gmail.com>
2024-03-31 12:36:01 +0300

    map - ispravlena pererisovka karty pri vybore goroda

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,47 +1,38 @@
 import {useRef, useEffect} from 'react';
-import {Icon, Marker, layerGroup} from 'leaflet';
+// import {Icon, LayerGroup, Marker, layerGroup} from 'leaflet';
 import useMap from '../../hooks/use-map';
-// import us
-// import {City, Points, Point} from '../../types/types';
-// import { TOfferCity, TPreviewOffer, TPreviewOffers } from '../../types/offer';
 import { TOfferCity, TPreviewOffers } from '../../types/offer';
-// import {URL_MARKER_DEFAULT, URL_MARKER_CURRENT} from '../../const';
 import 'leaflet/dist/leaflet.css';
-
-// type MapProps = {
-//   city: City;
-//   points: Points;
-//   selectedPoint: Point | undefined;
-// };
 
 type MapProps = {
   className?: string;
   city: TOfferCity;
-  // points: TPreviewOffers;
-  // selectedPoint: TPreviewOffer | undefined;
   offers: TPreviewOffers;
   activeOfferId?: string | null;
 };
 
 // const defaultCustomIcon = new Icon({
-//   iconUrl: URL_MARKER_DEFAULT,
-//   iconSize: [40, 40],
-//   iconAnchor: [20, 40]
+//   iconUrl: 'img/pin.svg',
+//   iconSize: [27, 39],
+//   iconAnchor: [13.5, 39]
 // });
 
 // const currentCustomIcon = new Icon({
-//   iconUrl: URL_MARKER_CURRENT,
-//   iconSize: [40, 40],
-//   iconAnchor: [20, 40]
+//   iconUrl: 'img/pin-active.svg',
+//   iconSize: [27, 39],
+//   iconAnchor: [13.5, 39]
 // });
 
-const defaultCustomIcon = new Icon({
+// вариант из открытого занятия
+import leaflet, {LayerGroup} from 'leaflet';
+
+const defaultCustomIcon = new leaflet.Icon({
   iconUrl: 'img/pin.svg',
   iconSize: [27, 39],
   iconAnchor: [13.5, 39]
 });
 
-const currentCustomIcon = new Icon({
+const currentCustomIcon = new leaflet.Icon({
   iconUrl: 'img/pin-active.svg',
   iconSize: [27, 39],
   iconAnchor: [13.5, 39]
@@ -53,69 +44,56 @@ export default function Map(props: MapProps): JSX.Element {
   const mapRef = useRef(null);
   const map = useMap(mapRef, city);
 
+  // вариант из открытого занятия
+  const markerLayer = useRef<LayerGroup>(leaflet.layerGroup());
+
   useEffect(() => {
     if (map) {
-      const markerLayer = layerGroup().addTo(map);
-      offers.forEach((offer) => {
-        const marker = new Marker({
-          // lat: point.lat,
-          // lng: point.lng
-          lat: offer.location.latitude,
-          lng: offer.location.longitude
-        });
-
-        marker
-          .setIcon(
-            // selectedPoint !== undefined && point.title === selectedPoint.title ? currentCustomIcon : defaultCustomIcon
-            offer.id === activeOfferId ? currentCustomIcon : defaultCustomIcon
-          )
-          .addTo(markerLayer);
-      });
-
-      return () => {
-        map.removeLayer(markerLayer);
-      };
+      map.setView([city.location.latitude, city.location.longitude], city.location.zoom);
+      markerLayer.current.addTo(map);
+      markerLayer.current.clearLayers();
     }
-  }, [map, offers, activeOfferId]);
+  }, [city, map]);
+
+  useEffect(() => {
+    if (map) {
+      offers.forEach((offer) => {
+        leaflet.marker(
+          {
+            lat: offer.location.latitude,
+            lng: offer.location.longitude
+          }
+        )
+        .setIcon(offer.id === activeOfferId ? currentCustomIcon : defaultCustomIcon)
+        .addTo(markerLayer.current);
+      });
+    }
+  });
+
+  // вариант из демо Академии
+  // useEffect(() => {
+  //   if (map) {
+  //     const markerLayer = layerGroup().addTo(map);
+  //     offers.forEach((offer) => {
+  //       const marker = new Marker({
+  //         lat: offer.location.latitude,
+  //         lng: offer.location.longitude
+  //       });
+
+  //       marker
+  //         .setIcon(
+  //           // selectedPoint !== undefined && point.title === selectedPoint.title ? currentCustomIcon : defaultCustomIcon
+  //           offer.id === activeOfferId ? currentCustomIcon : defaultCustomIcon
+  //         )
+  //         .addTo(markerLayer);
+  //     });
+
+  //     return () => {
+  //       map.removeLayer(markerLayer);
+  //     };
+  //   }
+  // }, [map, offers, activeOfferId]);
 
   // return <div style={{height: '500px'}} ref={mapRef}></div>;
   return <section className={`map ${className}`} ref={mapRef} />;
 }
-
-// export default Map;
-
-// export default function Map({city, offers, activeOfferId} : MapProps): JSX.Element {
-
-//   // const mapRef = useRef(null);
-//   // const map = useMap(mapRef, city);
-//   const mapRef = useRef<HTMLDivElement>(null);
-//   const map = useMap({location: city.location, containerRef: mapRef});
-
-//   useEffect(() => {
-//     if (map) {
-//       const markerLayer = layerGroup().addTo(map);
-//       offers.forEach((offer) => {
-//         const marker = new Marker({
-//           // lat: point.lat,
-//           // lng: point.lng
-//           lat: offer.location.latitude,
-//           lng: offer.location.longitude
-//         });
-
-//         marker
-//           .setIcon(
-//             activeOfferId !== undefined && point.title === selectedPoint.title
-//               ? currentCustomIcon
-//               : defaultCustomIcon
-//           )
-//           .addTo(markerLayer);
-//       });
-
-//       return () => {
-//         map.removeLayer(markerLayer);
-//       };
-//     }
-//   }, [map, points, selectedPoint]);
-
-//   return <div style={{height: '500px'}} ref={mapRef}></div>;
-// }
